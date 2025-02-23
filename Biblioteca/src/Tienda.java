453933110b9dafbb6b188627a13c86937557a9e5
RICARDO <RICARDO03-03-02@HOTMAIL.com>
2025-02-24 00:49:21 +0100

    prueba

---
+++ Biblioteca/src/Tienda.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Tienda {
+
+    ArrayList<Libro> libros = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
+    public void Menu1() {
+        System.out.println("|       BIENVENIDO       |");
+        System.out.println("|1. Mostrar Libros       |");
+        System.out.println("|2. Añadir Libro         |");
+        System.out.println("|3. Editar datos de libro|");
+        System.out.println("|4. Eliminar un libro    |");
+        System.out.println("|5. Filtrar por nombre   |");
+        System.out.println("|6. Salir                |");
+    }
+    public void Menu2() {
+        System.out.println("|   MODIFICAR DATOS   |");
+        System.out.println("|1. Titulo            |");
+        System.out.println("|2. Autor             |");
+        System.out.println("|3. Año de publicacion|");
+        System.out.println("|4. Numero de paginas |");
+        System.out.println("|5. Precio            |");
+        System.out.println("|6. Volver al inicio  |");
+    }
+
+    public void MostrarLibros() {
+        for(Libro libro : libros) {
+            System.out.println(libro.toString());
+        }
+    }
+    public void MostrarLibroAbreviado() {
+        for(int i = 0; i < libros.size(); i++) {
+            System.out.println((i+1)+" "+libros.get(i).getTitulo()+" $"+libros.get(i).getPrecio());
+        }
+    }
+
+    public void EliminarLibro() {
+        MostrarLibroAbreviado();
+        int pos = Integer.parseInt(sc.nextLine());
+        while(pos > libros.size()) {
+            System.out.println("Posicion invalida, vuelve a intentarlo");
+            pos = Integer.parseInt(sc.nextLine());
+        }
+        libros.remove(pos-1);
+        System.out.println("Libro eliminado correctamente");
+    }
+
+    public void AgregarLibro() {
+
+    }
+}
