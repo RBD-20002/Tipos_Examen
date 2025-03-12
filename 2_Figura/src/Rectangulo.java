f3b786d3ab3c83acda9e07a64ad2b94fbee632e9
Ricardo_bautista <ricardo03-03-02@hotmail.com>
2025-03-12 17:08:11 +0100

    Probate

---
+++ 2_Figura/src/Rectangulo.java
@@ -0,0 +1,15 @@
+public class Rectangulo extends Figura {
+
+    private final double base;
+    private final double altura;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return base * altura;
+    }
+}
